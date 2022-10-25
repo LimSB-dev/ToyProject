39a6bd17b494f2ecb247f8675d3b043146c89802
LimSB-dev <mae03087@naver.com>
2022-10-25 17:37:56 +0900

    Django CRUD

---
+++ articles/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+
+
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    input = models.IntegerField()
+    count = models.IntegerField()
+
